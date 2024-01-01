bf2f76beae6c31f67e31bc7f3dbcafc6815166f4
Borna Bhowmik <124670440+bornabhowmik@users.noreply.github.com>
2024-01-01 23:50:12 +0600

    add input-output.cpp code

---
+++ input-output.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Enter Your Nickname: ";
+    string s;
+    cin >> s;
+
+    cout << "Hello! " + s << endl;
+    cout << "Welcome to Programming World!";
+
+
+    return 0;
+}
