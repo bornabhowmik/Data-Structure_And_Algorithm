675a3e66d1513cef0baf0bf534e986de5f039ba0
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-18 00:36:09 +0600

    pattern2.cpp code added

--- Pattern/pattern2.cpp
+++ Pattern/pattern2.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -6,17 +6,18 @@ int main() {
     int n;
     cin>>n;
 
-    int i = 1;
+    int i=1;
 
     while(i<=n){
-        int j = 1;
+        int j=1;
         while(j<=n){
-            cout<<"*";
+            cout<<i;
             j = j+1;
         }
-        i = i+1;
+
         cout<<endl;
-        // i = i+1;
+        i = i+1;
+        // cout<<endl;
     }
 
     return 0;
