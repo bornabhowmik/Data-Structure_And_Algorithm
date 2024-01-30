8f45eb2e85e1f15794e3ea51d3a609b2121d2880
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-30 23:53:58 +0600

    pattern6.cpp code added

---
+++ Pattern/input.cpp
@@ -0,0 +1,28 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+    
+//     int n;
+//     cin>>n;
+
+//     printf("%d", n);
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int n;
+    cin>>n;
+	
+	int n = x*y;
+	
+	cout<<n<<endl;
+	
+	return 0;
+
+}
