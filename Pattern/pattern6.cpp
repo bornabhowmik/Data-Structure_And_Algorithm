cd34e6404def6f6af062f6233cf47607e124af6e
bornabhowmik <bornabhowmik4@gmail.com>
2024-02-14 23:45:42 +0600

    update code added

--- Pattern/pattern6.cpp
+++ Pattern/pattern6.cpp
@@ -6,19 +6,21 @@ int main() {
     int n;
     cin>>n;
 
-    int row = 1;
+    int i = 1;
+    int count = 1;
 
-    while(row<=n){
+    while(i<=n){
 
-        int col = 1;
-        while(col<=row)
+        int j = 1;
+        while(j<=n)
         {
-            cout<<row<<" ";
-            col = col+1;
+            cout<<count<<" ";
+            count = count+1;
+            j = j+1;
         }
 
-        cout<<endl;
-        row = row+1;
+        // cout<<endl;
+        // i = i+1;
     }
 
     return 0;
